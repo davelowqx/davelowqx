1c29ec4bea5d35d2c874a17a148c8d1d787396c2
davelowqx <lowqixiang@gmail.com>
2021-11-30 12:43:42 +0800

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,184 +1,203 @@
-import * as React from "react"
-
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
+import React from "react";
+import "../styles/global.css";
 
 // data
+const projects = [
+  {
+    title: "finatic",
+    url: "finatic.vercel.app",
+    summary:
+      "Next.js, semantic UI, Firebase Firestore/Storage, web3.js/MetaMask, Solidity, Ganache CLI, mocha",
+    description:
+      "Decentralised application on the Ethereum network, where companies can issue tokenized shares for investors.",
+    badge: "unmaintained",
+    badgeColor: "red",
+    duration: "Jun 2021 - Aug 2021",
+  },
+  {
+    title: "signaci",
+    url: "signaci.com",
+    summary: "Next.js, tailwindcss, PostgreSQL, Firebase Auth",
+    description:
+      "Application for traders to post and follow trade ideas, with their performance tracked.",
+    badge: "development",
+    badgeColor: "gray",
+    duration: "Aug 2021 - Present",
+  },
+  {
+    title: "bloomburger",
+    url: "bloomburger.co",
+    summary:
+      "Next.js, Bootstrap, Lightweight Charts, React Table, Yahoo Finance/FTX API",
+    description:
+      "Dashboard for traders to monitor financial markets, inspired by the Bloomberg Terminal.",
+    badge: "production",
+    badgeColor: "green",
+    duration: "Jun 2021 - Present",
+  },
+  {
+    title: "DriveSafeSG",
+    url: "drivesafesg.vercel.app",
+    summary: "webpack, Google Maps Platform",
+    description:
+      "Web application to map the location of speed cameras and signs in Singapore.",
+    badge: "production",
+    badgeColor: "green",
+    duration: "Oct 2021 - Present",
+  },
+  {
+    title: "carousearch",
+    url: "carousearch.vercel.app",
+    summary: "Next.js, tailwind-css",
+    description:
+      "Frontend to search carousell.com with additional functionality and metrics.",
+    badge: "production",
+    badgeColor: "green",
+    duration: "Oct 2021 - Oct 2021",
+  },
+  {
+    title: "fuzzyfilms",
+    url: "fuzzyfilms.co",
+    summary: "Gatsby.js, styled-components, Formik",
+    description: "Static landing page site.",
+    badge: "production",
+    badgeColor: "green",
+    duration: "Jun 2021 - Jul 2021",
+  },
+];
+
+const skills = [
+  {
+    title: "Languages",
+    description:
+      "HTML, CSS, JavaScript, TypeScript, SQL, Java, Solidity, C, Python, Markdown",
+  },
+  {
+    title: "Stacks",
+    description:
+      "Node.js, React.js, Express.js, Next.js, PostgreSQL, web3.js/MetaMask",
+  },
+  {
+    title: "Tools",
+    description:
+      "Git, Vim, VSCode, IntelliJ IDEA, Postman, Chrome DevTools, Figma",
+  },
+  {
+    title: "Services",
+    description: "AWS (RDS), GCP, Firebase, Vercel, Cloudfare, Heroku, Netlify",
+  },
+];
+
+const education = {
+  school: "National University of Singapore",
+  duration: "Aug 2020 - May 2024",
+  degree: "Computer Science (Hons.), Minor in Management, CAP 4.2",
+  details: [
+    {
+      title: "Coursework",
+      description:
+        "Data Structures & Algorithms, Software Engineering, Operating Systems, Database, Finance",
+    },
+    {
+      title: "Achievements",
+      description: "NUS Merit Scholarship, Teaching Assistant",
+    },
+    {
+      title: "Activities",
+      description: "Basketball, Developers Club, Media Team, Fintech Society",
+    },
+  ],
+};
+
 const links = [
+  { title: "github.com/davelowqx", href: "https://github.com/davelowqx" },
   {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
+    title: "linkedin.com/in/davelowqx",
+    href: "https://linkedin.com/in/davelowqx",
   },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+];
+
+const Detail = ({ title, description }) => (
+  <div>
+    <span className="font-semibold">{" • " + title}: </span>
+    {description}
+  </div>
+);
 
 // markup
 const IndexPage = () => {
+  const [showEmail, setShowEmail] = React.useState(false);
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
+    <div className="m-6">
+      <div className="mx-auto max-w-3xl leading-snug">
+        <h1 className="font-bold text-2xl">DAVE LOW</h1>
+        <p className="font-light">
           <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
+            href={showEmail && "mailto:hello@davelowqx.com"}
+            className={!showEmail && `bg-gray-100`}
+            onClick={() => !showEmail && setShowEmail(true)}
           >
-            {docLink.text}
+            {showEmail ? "hello@davelowqx.com" : "email"}
           </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
+          {links.map(({ title, href }, i) => (
+            <span key={i}>
+              {" | "}
+              <a href={href}>{title}</a>
             </span>
-          </li>
+          ))}
+        </p>
+        <br />
+        <h2 className="font-bold text-lg">EDUCATION</h2>
+        <hr className="mb-2" />
+        <div className="flex">
+          <h3 className="font-bold">{education.school}</h3>
+          <div className="flex-grow" />
+          <div className="font-light">{education.duration}</div>
+        </div>
+        <div className="italic">{education.degree}</div>
+        <div>
+          {education.details.map((detail) => (
+            <Detail title={detail.title} description={detail.description} />
+          ))}
+        </div>
+        <br />
+        <h2 className="font-bold text-lg">SKILLS</h2>
+        <hr className="mb-2" />
+        {skills.map((skill) => (
+          <Detail title={skill.title} description={skill.description} />
         ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
-}
 
-export default IndexPage
+        <br />
+        <h2 className="font-bold text-lg">PROJECTS</h2>
+        <hr className="mb-2" />
+        <ul>
+          {projects.map((project) => (
+            <div className="mb-2" key={project.url}>
+              <span>
+                <div className="flex">
+                  <a className="font-semibold" href={`https://${project.url}`}>
+                    {project.title}
+                    <span className="font-light ml-1 underline">
+                      {project.url}
+                    </span>
+                  </a>
+                  <div
+                    className={`bg-${project.badgeColor}-100 ml-1 font-light`}
+                  >
+                    {project.badge}
+                  </div>
+                  <div className="flex-grow" />
+                  <div className="font-light">{project.duration}</div>
+                </div>
+                <p>{project.summary}</p>
+                <p className="font-light">{project.description}</p>
+              </span>
+            </div>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default IndexPage;

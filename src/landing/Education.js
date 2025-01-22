f00f04fcd8b00886a96e0f01faeb80d70cafc8c4
Dave Low <67348776+davelowqx@users.noreply.github.com>
2025-01-22 12:04:09 +0800

    Update Education.js

--- src/landing/Education.js
+++ src/landing/Education.js
@@ -4,19 +4,8 @@ export default function Education() {
   const education = {
     school: 'National University of Singapore',
     duration: 'Aug 2020 - Dec 2024',
-    degree:
-      'Computer Science (Focus on Algorithms / Parallel Computing)',
-    details: [
-      {
-        title: 'Achievements',
-        description: 'NUS Merit Scholarship',
-      },
-      {
-        title: 'Extracurriculars',
-        description:
-          'Media, Basketball, Dance',
-      },
-    ],
+    degree: 'Computer Science (Hons)',
+    details: 'Algorithms/Parallel Computing focus, NUS Merit Scholar',
   }
   return (
     <>
@@ -24,20 +13,11 @@ export default function Education() {
       <hr className="mb-2.5" />
       <div className="flex">
         <h3 className="font-bold">{education.school}</h3>
-
         <div className="flex-grow" />
         <div className="font-light hidden xs:block">{education.duration}</div>
       </div>
       <div className="italic">{education.degree}</div>
-      <div>
-        {education.details.map((detail, i) => (
-          <Bullet
-            key={i}
-            title={detail.title}
-            description={detail.description}
-          />
-        ))}
-      </div>
+      <p>{education.details}</p>
     </>
   )
 }

0bc54e0d7ad2096aa7bd21130bafbba4a91f4bf3
Dave Low <67348776+davelowqx@users.noreply.github.com>
2025-08-21 15:26:23 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Education, Experience, Skills, Projects, Title } from '../landing'
+import { Title, Summary, Experience, Education, Projects, Skills } from '../landing'
 import Seo from '../components/Seo'
 import '../styles/global.css'
 
@@ -11,6 +11,8 @@ const IndexPage = () => {
       <div className="m-6">
         <div className="mx-auto max-w-3xl leading-snug text-gray-900">
           <Title />
+          <br />
+          <Summary />
           <br />
           <Experience />
           <br />

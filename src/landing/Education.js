2a1e843e1194766c399926ab7fbdcf25f027ba93
Dave Low <67348776+davelowqx@users.noreply.github.com>
2025-08-21 14:16:49 +0800

    Update Education.js

--- src/landing/Education.js
+++ src/landing/Education.js
@@ -2,22 +2,22 @@ import React from 'react'
 import Bullet from '../components/Bullet'
 export default function Education() {
   const education = {
-    school: 'National University of Singapore',
-    duration: 'Aug 2020 - Dec 2024',
-    degree: 'Computer Science (Hons)',
-    details: 'Algorithms/Parallel Computing focus, NUS Merit Scholar',
+    title: 'Computer Science',
+    duration: 'Graduated Dec 2024',
+    details1: 'National University of Singapore',
+    details2: 'Algorithms/Parallel Computing focus, NUS Merit Scholar',
   }
   return (
     <>
       <h2 className="font-bold text-lg">EDUCATION</h2>
       <hr className="mb-2.5" />
       <div className="flex">
-        <h3 className="font-bold">{education.school}</h3>
+        <h3 className="font-semi-bold">{education.title}</h3>
         <div className="flex-grow" />
         <div className="font-light hidden xs:block">{education.duration}</div>
       </div>
-      <div className="italic">{education.degree}</div>
-      <p>{education.details}</p>
+      <div className="italic">{education.details1}</div>
+      <p>{education.details2}</p>
     </>
   )
 }

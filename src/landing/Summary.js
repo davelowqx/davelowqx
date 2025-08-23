34b2b95f1bd309b1162a5d36e14031ceadd7e2ab
Dave Low <67348776+davelowqx@users.noreply.github.com>
2025-08-23 10:29:54 +0800

    Update Summary.js

--- src/landing/Summary.js
+++ src/landing/Summary.js
@@ -1,6 +1,6 @@
 import React from 'react'
 export default function Summary() {
-  const summary = "Obsessed with solving problems, from debugging complex software to restoring classic cars. Always seeking growth and meaningful challenges in pursuit of a life worth living.";
+  const summary = "I'm deeply obsessed with solving problems, particularly in the intersection of financial markets and low-latency systems. Constantly in pursuit of new challenges and dedicated to lifelong learning.";
   return (
     <>
       <h2 className="font-bold text-lg">SUMMARY</h2>

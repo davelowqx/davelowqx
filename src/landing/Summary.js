2aabdee6bf18f8591b5eb259081ae7bcc0044b9a
Dave Low <67348776+davelowqx@users.noreply.github.com>
2026-03-10 02:38:56 +0800

    Update Summary.js

--- src/landing/Summary.js
+++ src/landing/Summary.js
@@ -1,11 +1,5 @@
 import React from 'react'
 export default function Summary() {
   const summary = "Quantitative Developer passionate about solving problems in the intersection of financial markets and low latency systems. Python/C++.";
-  return (
-    <>
-      <h2 className="font-bold text-lg">SUMMARY</h2>
-      <hr className="mb-2.5" />
-      <h3>{summary}</h3>
-    </>
-  )
+  return (<h3>{summary}</h3>)
 }

4b4cfb1e7bed1351f6aa04137a1d1140697e0842
Dave Low <67348776+davelowqx@users.noreply.github.com>
2026-03-20 01:50:49 +0800

    Update Summary.js

--- src/landing/Summary.js
+++ src/landing/Summary.js
@@ -1,5 +1,5 @@
 import React from 'react'
 export default function Summary() {
-  const summary = "Quantitative Developer passionate about solving problems in the intersection of financial markets and low latency systems. Python/C++.";
+  const summary = "Quantitative Developer passionate about solving problems in the intersection of financial markets and low latency systems. Python/C++/Go/SQL, Polars/Redis/Kafka/Docker/Kubernetes";
   return (<h3>{summary}</h3>)
 }

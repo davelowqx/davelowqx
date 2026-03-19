1b491643d8470d22da7891a14600cd62b5b0b201
Dave Low <67348776+davelowqx@users.noreply.github.com>
2026-03-20 02:15:19 +0800

    Update index.js

--- src/landing/index.js
+++ src/landing/index.js
@@ -2,6 +2,4 @@ import Title from './Title'
 import Summary from './Summary'
 import Experience from './Experience'
 import Education from './Education'
-import Projects from './Projects'
-import Skills from './Skills'
-export { Title, Summary, Experience, Education, Projects, Skills }
+export { Title, Summary, Experience, Education }

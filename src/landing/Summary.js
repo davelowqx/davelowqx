a1ffb97d187e284ad5c8677aa5d086e6a16ae058
Dave Low <67348776+davelowqx@users.noreply.github.com>
2026-03-20 02:10:23 +0800

    Update Summary.js

--- src/landing/Summary.js
+++ src/landing/Summary.js
@@ -2,7 +2,7 @@ import React from 'react'
 export default function Summary() {
   const summary = "Quantitative Developer passionate about solving problems in the intersection of financial markets and low latency systems.";
   const languages = "Python, C++, Go";
-  const technologies = "Redis, Kafka, Docker, Kubernetes, Terraform, Polars";
+  const technologies = "Redis, Kafka, Docker, Kubernetes, Terraform, Elasticsearch, Polars";
   return (
     <>
     <h3>{summary}</h3>
